a0861a0e8d70a0d801ccc7e4f93a85705dba4a22
Ilija Vukotic <ivukotic@cern.ch>
2020-12-16 12:21:47 -0600

    missed json

--- utils/helpers.py
+++ utils/helpers.py
@@ -2,6 +2,7 @@ from datetime import datetime, timedelta
 import dateutil.relativedelta
 import time
 import os
+import json
 
 from elasticsearch import Elasticsearch
 from elasticsearch.helpers import scan
@@ -31,15 +32,20 @@ def timer(func):
 
 
 '''Returns a period of the past 3 hours'''
+
+
 def defaultTimeRange(hours=3):
     now = datetime.utcnow()
     defaultEnd = datetime.strftime(now, '%Y-%m-%d %H:%M')
-    defaultStart = datetime.strftime(now - timedelta(hours=hours), '%Y-%m-%d %H:%M')
+    defaultStart = datetime.strftime(
+        now - timedelta(hours=hours), '%Y-%m-%d %H:%M')
 
     return [defaultStart, defaultEnd]
 
 
 '''Finds the difference between two dates'''
+
+
 def FindPeriodDiff(dateFrom, dateTo):
     fmt = '%Y-%m-%d %H:%M'
     d1 = datetime.strptime(dateFrom, fmt)
@@ -50,18 +56,22 @@ def FindPeriodDiff(dateFrom, dateTo):
 
 
 '''Splits the period into chunks of specified number of intervals.'''
+
+
 def GetTimeRanges(dateFrom, dateTo, intv=1):
     diff = FindPeriodDiff(dateFrom, dateTo) / intv
     t_format = "%Y-%m-%d %H:%M"
     tl = []
     for i in range(intv+1):
-        t = (datetime.strptime(dateFrom,t_format) + diff * i).strftime(t_format)
+        t = (datetime.strptime(dateFrom, t_format) + diff * i).strftime(t_format)
         tl.append(int(time.mktime(datetime.strptime(t, t_format).timetuple())*1000))
 
     return tl
 
 
 '''The following method helps to calculate the expected number of tests for a specific period'''
+
+
 def CalcMinutes4Period(dateFrom, dateTo):
     time_delta = FindPeriodDiff(dateFrom, dateTo)
 
