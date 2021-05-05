eab450de072fed3926d70cd63064018bfb3330f5
petya-vasileva <petya.tsv.vasileva@gmail.com>
2021-05-05 14:56:11 +0000

    Update code and use the new alarm types - 'Firewall issue' and 'high packet loss on multiple links'

--- ps-packetloss.py
+++ ps-packetloss.py
@@ -2,8 +2,8 @@ from alarms import alarms
 import pandas as pd
 
 from data_objects.DataLoader import PrtoblematicPairsDataLoader
-
 pls = PrtoblematicPairsDataLoader('ps_packetloss')
+
 
 """
 'src', 'src_host', 'src_site':      info about the source
@@ -16,12 +16,47 @@ pls = PrtoblematicPairsDataLoader('ps_packetloss')
 cols = ['src', 'dest', 'src_host', 'dest_host',
         'src_site', 'dest_site', 'avg_value', 'tests_done']
 
+
 ##### High packet loss #####
 
-sign_ploss = pls.df[(pls.df['flag'] == 1)][cols].to_dict(orient='records')
-ALARM = alarms('Networking', 'Perfsonar', 'high packet loss')
-for item in sign_ploss:
-    ALARM.addAlarm(body='Link shows high packet loss', tags=[], source=item)
+sign_ploss = pls.df[(pls.df['flag'] == 1)][cols]
+
+
+# Loop over all src/dest_hosts and grab the sites they show problems with
+data = {}
+for host in list(set(sign_ploss['src_host'].to_list())):
+    site = sign_ploss[(sign_ploss['src_host']==host)]['src_site'].values[0]
+    dest_sites = sign_ploss[sign_ploss['src_host']==host]['dest_site'].values.tolist()
+    data[host] = {"site":site, "host":host, "dest_sites":dest_sites, "src_sites":[]}
+
+for host in list(set(sign_ploss['dest_host'].to_list())):
+    site = sign_ploss[(sign_ploss['dest_host']==host)]['dest_site'].values[0]
+    src_sites = sign_ploss[sign_ploss['dest_host']==host]['src_site'].values.tolist()
+    if host in data.keys():
+        data[host]["src_sites"] = src_sites
+    else: 
+        data[host] = {"site":site, "host":host, "dest_sites":[], "src_sites":src_sites}
+
+# Create the alarm types
+alarmOnList = alarms('Networking', 'Perfsonar', 'high packet loss on multiple links')
+alarmOnPair = alarms('Networking', 'Perfsonar', 'high packet loss')
+
+# Loop over all hosts and generate alarms depending on the number of sites it shows problems with
+for key, item in data.items():
+    cnt = len(item['src_sites']) + len(item['dest_sites'])
+
+    # If the host shows problems in one direction or both to more than 5 other sites, send an alarm with lists of sites
+    if cnt>=5:
+        alarmOnList.addAlarm(body='Host shows high packet loss to multiple sites', tags=[item['site']], source=item)
+    else:
+    # Otherwise send a standard alarm including more fields
+        for src in data[key]['src_sites']:
+            doc = sign_ploss[(sign_ploss['dest_host']==key) & (sign_ploss['src_site']==src)][cols].to_dict(orient='records')[0]
+            alarmOnPair.addAlarm(body='Link shows high packet loss', tags=[doc['src_site'], doc['dest_site']], source=doc)
+        for dest in data[key]['dest_sites']:
+            doc = sign_ploss[(sign_ploss['src_host']==key) & (sign_ploss['dest_site']==dest)][cols].to_dict(orient='records')[0]
+            alarmOnPair.addAlarm(body='Link shows high packet loss', tags=[doc['src_site'], doc['dest_site']], source=doc)
+
 
 
 ##### Complete packet loss #####
@@ -31,10 +66,10 @@ for item in sign_ploss:
 # under 'Firewall issue' event type
 
 complete_ploss = pls.df[(pls.df['flag'] == 2)]
-ALARM = alarms('Networking', 'Perfsonar', 'Firewall issue')
+alarmFirewall = alarms('Networking', 'Perfsonar', 'Firewall issue')
 
 # Get all destination hosts that have more than 5 source hosts 
-groups = df.groupby(['dest_host']).agg({'src_host': 'count'}).reset_index()
+groups = complete_ploss.groupby(['dest_host']).agg({'src_host': 'count'}).reset_index()
 destWhereCntGrTh5hosts = groups[groups['src_host']>5]['dest_host'].values
 
 if len(destWhereCntGrTh5hosts)>0:
@@ -42,25 +77,11 @@ if len(destWhereCntGrTh5hosts)>0:
         site = complete_ploss[complete_ploss['dest_host']==host]['dest_site'].unique()[0]
         site_list = complete_ploss[complete_ploss['dest_host']==host]['src_site'].values.tolist()
         data = {"site":site, "host":host, "sites":site_list}
-        ALARM.addAlarm(body='Firewall issue', tags=[site], source=data)
-
-
-
-# Get all source hosts that have more than 5 destination hosts 
-groups = df.groupby(['src_host']).agg({'dest_host': 'count'}).reset_index()
-srcWhereCntGrTh5hosts = groups[groups['dest_host']>5]['src_host'].values
-
-if len(srcWhereCntGrTh5hosts)>0:
-    for host in srcWhereCntGrTh5hosts:
-        site = complete_ploss[complete_ploss['src_host']==host]['src_site'].unique()[0]
-        site_list = complete_ploss[complete_ploss['src_host']==host]['dest_site'].values.tolist()
-        data = {"site":site, "host":host, "sites":site_list}
-        ALARM.addAlarm(body='Firewall issue', tags=[site], source=data)
+        alarmFirewall.addAlarm(body='Firewall issue', tags=[site], source=data)
 
 
 # Send the reamaining pairs under 'complete packet loss' event type
-blocked = complete_ploss[(~complete_ploss['dest_host'].isin(destWhereCntGrTh5hosts)) 
-               & (~complete_ploss['src_host'].isin(srcWhereCntGrTh5hosts))][cols].to_dict(orient='records')
-ALARM = alarms('Networking', 'Perfsonar', 'complete packet loss')
+blocked = complete_ploss[(~complete_ploss['dest_host'].isin(destWhereCntGrTh5hosts))][cols].to_dict(orient='records')
+alarmCompleteLoss = alarms('Networking', 'Perfsonar', 'complete packet loss')
 for item in blocked:
-    ALARM.addAlarm(body='Link shows complete packet loss', tags=[item['src_site'], item['dest_site']], source=item)
+    alarmCompleteLoss.addAlarm(body='Link shows complete packet loss', tags=[item['src_site'], item['dest_site']], source=item)
