98b045e884a55c05ed1c232444b4d1151ef0d8e8
root <petya.tsv.vasileva@gmail.com>
2022-02-19 14:22:51 +0000

    Add IP version

--- ps-throughput.py
+++ ps-throughput.py
@@ -15,7 +15,122 @@ from alarms import alarms
 
 
 
-def fixMissingMetadata(rowDf, idx):
+def query4Avg(idx, dateFrom, dateTo):
+    query = {
+              "size" : 0,
+              "query" : {
+                "bool" : {
+                  "must" : [
+                    {
+                      "range" : {
+                        "timestamp" : {
+                          "gt" : dateFrom,
+                          "lte": dateTo
+                        }
+                      }
+                    },
+                    {
+                      "term" : {
+                        "src_production" : True
+                      }
+                    },
+                    {
+                      "term" : {
+                        "dest_production" : True
+                      }
+                    }
+                  ]
+                }
+              },
+              "aggregations" : {
+                "groupby" : {
+                  "composite" : {
+                    "size" : 9999,
+                    "sources" : [
+                      {
+                        "ipv6" : {
+                          "terms" : {
+                            "field" : "ipv6"
+                          }
+                        }
+                      },
+                      {
+                        "src" : {
+                          "terms" : {
+                            "field" : "src"
+                          }
+                        }
+                      },
+                      {
+                        "dest" : {
+                          "terms" : {
+                            "field" : "dest"
+                          }
+                        }
+                      },
+                      {
+                        "src_host" : {
+                          "terms" : {
+                            "field" : "src_host"
+                          }
+                        }
+                      },
+                      {
+                        "dest_host" : {
+                          "terms" : {
+                            "field" : "dest_host"
+                          }
+                        }
+                      },
+                      {
+                        "src_site" : {
+                          "terms" : {
+                            "field" : "src_site"
+                          }
+                        }
+                      },
+                      {
+                        "dest_site" : {
+                          "terms" : {
+                            "field" : "dest_site"
+                          }
+                        }
+                      }
+                    ]
+                  },
+                  "aggs": {
+                    "throughput": {
+                      "avg": {
+                        "field": "throughput"
+                      }
+                    }
+                  }
+                }
+              }
+            }
+
+
+#     print(idx, str(query).replace("\'", "\""))
+    aggrs = []
+
+    aggdata = hp.es.search(index=idx, body=query)
+    for item in aggdata['aggregations']['groupby']['buckets']:
+        aggrs.append({'hash': str(item['key']['src']+'-'+item['key']['dest']),
+                      'from':dateFrom, 'to':dateTo,
+                      'ipv6': item['key']['ipv6'],
+                      'src': item['key']['src'], 'dest': item['key']['dest'],
+                      'src_host': item['key']['src_host'], 'dest_host': item['key']['dest_host'],
+                      'src_site': item['key']['src_site'], 'dest_site': item['key']['dest_site'],
+                      'value': item['throughput']['value'],
+                      'doc_count': item['doc_count']
+                     })
+
+    return aggrs
+
+
+
+
+def fixMissingMetadata(rawDf, idx):
     metadf = NodesMetaData(idx, dateFrom, dateTo).df
     df1 = pd.merge(metadf[['host', 'ip', 'site']], rowDf[[
                    'src', 'hash']], left_on='ip', right_on='src', how='right')
@@ -37,7 +152,7 @@ def queryData(idx, dateFrom, dateTo):
     intv = int(hp.CalcMinutes4Period(dateFrom, dateTo)/60)
     time_list = hp.GetTimeRanges(dateFrom, dateTo, intv)
     for i in range(len(time_list)-1):
-        data.extend(qrs.query4Avg(idx, time_list[i], time_list[i+1]))
+        data.extend(query4Avg(idx, time_list[i], time_list[i+1]))
 
     return data
 
@@ -49,7 +164,7 @@ def getStats(df, threshold):
     metaDf['value'] = round(metaDf['value']*1e-6)
     
     # split the data in 3 days
-    sitesDf = metaDf.groupby(['src_site', 'dest_site',pd.Grouper(key='dt', freq='4d')])['value'].mean().to_frame().reset_index()
+    sitesDf = metaDf.groupby(['src_site', 'dest_site','ipv6', pd.Grouper(key='dt', freq='4d')])['value'].mean().to_frame().reset_index()
     
     # get the statistics
     sitesDf['z'] = sitesDf.groupby(['src_site','dest_site'])['value'].apply(lambda x: round((x - x.mean())/x.std(),2))
@@ -71,9 +186,9 @@ def getStats(df, threshold):
 def createAlarms(alarmsDf, alarmType, minCount=5):
     # we aim for exposing a single site which shows significant change in throughput from/to 5 (default value) other sites in total
     # below we find the total count of unique sites related to a single site name
-    src_cnt = alarmsDf[['src_site']].value_counts().to_frame().reset_index().rename(columns={0:'cnt', 'src_site': 'site'})
-    dest_cnt = alarmsDf[['dest_site']].value_counts().to_frame().reset_index().rename(columns={0:'cnt', 'dest_site': 'site'})
-    cntDf = pd.concat([src_cnt, dest_cnt]).groupby(['site']).sum().reset_index()
+    src_cnt = alarmsDf[['src_site','ipv6']].value_counts().to_frame().reset_index().rename(columns={0:'cnt', 'src_site': 'site'})
+    dest_cnt = alarmsDf[['dest_site','ipv6']].value_counts().to_frame().reset_index().rename(columns={0:'cnt', 'dest_site': 'site'})
+    cntDf = pd.concat([src_cnt, dest_cnt]).groupby(['site','ipv6']).sum().reset_index()
 
     # create the alarm objects
     alarmOnPair = alarms('Networking', 'Perfsonar', alarmType)
@@ -81,10 +196,10 @@ def createAlarms(alarmsDf, alarmType, minCount=5):
 
     rows2Delete = []
 
-    for site in cntDf[cntDf['cnt']>=minCount]['site'].values:
+    for site, ipv6 in cntDf[cntDf['cnt']>=minCount][['site','ipv6']].values:
 
-        subset = alarmsDf[(alarmsDf['src_site']==site)|(alarmsDf['dest_site']==site)]
-        
+        subset = alarmsDf[((alarmsDf['src_site']==site)|(alarmsDf['dest_site']==site))&(alarmsDf['ipv6']==ipv6)]
+
         # build the lists of values
         src_sites, dest_sites, src_change, dest_change = [],[],[],[]
         for idx, row in subset.iterrows():
@@ -94,9 +209,9 @@ def createAlarms(alarmsDf, alarmType, minCount=5):
             if row['dest_site'] != site:
                 dest_sites.append(row['dest_site'])
                 dest_change.append(row['%change'])
-        
+
         # create the alarm source content
-        doc = {'dest_sites':dest_sites, 'dest_change':dest_change, 'src_sites':src_sites, 'src_change':src_change}
+        doc = {'ipv6':ipv6,'dest_sites':dest_sites, 'dest_change':dest_change, 'src_sites':src_sites, 'src_change':src_change}
         doc['site'] = site
 
         # send the alarm with the proper message
@@ -107,7 +222,7 @@ def createAlarms(alarmsDf, alarmType, minCount=5):
     alarmsDf = alarmsDf.drop(rows2Delete)
 
     # The rest will be send as 'regular' src-dest alarms
-    for doc in alarmsDf[(alarmsDf['%change']<=-50)|(alarmsDf['%change']>=50)][['src_site', 'dest_site', 'last3days_avg', '%change']].to_dict('records'):
+    for doc in alarmsDf[(alarmsDf['%change']<=-50)|(alarmsDf['%change']>=50)][['src_site', 'dest_site', 'ipv6', 'last3days_avg', '%change']].to_dict('records'):
         alarmOnPair.addAlarm(body=alarmType, tags=[doc['src_site'], doc['dest_site']], source=doc)
 
 
