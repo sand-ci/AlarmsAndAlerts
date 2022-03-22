769ec2039bd76f81582bec1884ba77a075fc9947
Ilija Vukotic <ivukotic@uchicago.edu>
2022-03-21 22:23:19 -0500

    Update ps-clock-corrections.py

--- ps-clock-corrections.py
+++ ps-clock-corrections.py
@@ -19,9 +19,9 @@ with open('/config/config.json') as json_data:
     config = json.load(json_data,)
 
 es = Elasticsearch(
-    hosts=[{'host': config['ES_HOST'], 'scheme':'https'}],
+    hosts=[{'host': config['ES_HOST'], 'port':9200, 'scheme':'https'}],
     http_auth=(config['ES_USER'], config['ES_PASS']),
-    timeout=60)
+    request_timeout=60)
 
 es.ping()
 
