7be0874a37bd5dee1bbbfed74b1d629c88471ba5
petya-vasileva <petya.tsv.vasileva@gmail.com>
2022-05-20 20:16:25 +0000

    Add ASNs to ES

---
+++ ps-asns.py
@@ -0,0 +1,46 @@
+# # 
+# Downloads the Research & Education Autonomous System Numbers (ASNs)
+# and stores the information in ps_asns index in ES
+# #
+
+import requests
+from datetime import datetime
+from elasticsearch import Elasticsearch
+from elasticsearch.helpers import parallel_bulk
+
+import utils.helpers as hp
+
+def loadASNs():
+    response = requests.get("https://bgp.nsrc.org/REN/USP/data-used-autnums",verify=False)
+    content = response.text.strip()
+    lines = content.split('\n')
+    
+    asnDict = {}
+    try: 
+        for l in lines:
+            # print(l)
+            row = l.strip()
+            row = row.split(' ',1)
+            if not row[1] in ["UNALLOCATED", "UNASSIGNED"]:
+                asnDict[int(row[0])] = row[1]
+
+        return asnDict
+    except Exception as e:
+        print('Issue wtih:',e, row)
+
+
+def genData(asnInfo):
+    for asn, owner in asnInfo.items():
+        yield {
+            "_index": "ps_asns",
+            "_id": asn,
+            "owner": owner,
+            
+        }
+
+asnInfo = loadASNs()
+print(f'Number of ASNs to store: {len(asnInfo)}')
+
+for success, info in parallel_bulk(hp.es, genData(asnInfo)):
+    if not success:
+        print('A document failed:', info)
