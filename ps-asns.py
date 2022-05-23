ce7a3a18b3faf2820c6b77f807c5987f43eb02a0
Ilija Vukotic <ivukotic@cern.ch>
2022-05-23 09:32:08 -0500

    cleanup and add asns

--- ps-asns.py
+++ ps-asns.py
@@ -1,32 +1,31 @@
-# # 
+# #
 # Downloads the Research & Education Autonomous System Numbers (ASNs)
 # and stores the information in ps_asns index in ES
 # #
 
 import requests
-from datetime import datetime
-from elasticsearch import Elasticsearch
 from elasticsearch.helpers import parallel_bulk
 
 import utils.helpers as hp
 
+
 def loadASNs():
-    response = requests.get("https://bgp.nsrc.org/REN/USP/data-used-autnums",verify=False)
+    response = requests.get("https://bgp.nsrc.org/REN/USP/data-used-autnums", verify=False)
     content = response.text.strip()
     lines = content.split('\n')
-    
+
     asnDict = {}
-    try: 
+    try:
         for l in lines:
             # print(l)
             row = l.strip()
-            row = row.split(' ',1)
+            row = row.split(' ', 1)
             if not row[1] in ["UNALLOCATED", "UNASSIGNED"]:
                 asnDict[int(row[0])] = row[1]
 
         return asnDict
     except Exception as e:
-        print('Issue wtih:',e, row)
+        print('Issue wtih:', e, row)
 
 
 def genData(asnInfo):
@@ -35,8 +34,8 @@ def genData(asnInfo):
             "_index": "ps_asns",
             "_id": asn,
             "owner": owner,
-            
         }
+
 
 asnInfo = loadASNs()
 print(f'Number of ASNs to store: {len(asnInfo)}')
