0f45a74c5ef847408842d85e18c509e842d07889
Ilija Vukotic <ivukotic@uchicago.edu>
2026-01-07 09:39:46 -0600

    Enhance routers.sh script; ensure pip installation and update requirements before running routers.py

--- routers.sh
+++ routers.sh
@@ -1,7 +1,10 @@
 #!/bin/bash
+
 date
-python routers.py
+python3 -m ensurepip --upgrade
+python3 -m pip install -r requirements.txt
+python3 routers.py > routers.log 2>&1
 rc=$?; if [[ $rc != 0 ]]; then 
-    echo "problem running traceroute. Exiting."
+    echo "problem running routers alarm. Exiting."
     exit $rc
 fi
